8f11453ff0e954c04cbd538a13152db37ea75013
hyunwook <c004245@naver.com>
2018-06-06 17:40:07 +0900

    main menu , main view add

---
+++ app/src/main/java/com/example/hyunwook/schedulermacbooktroops/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hyunwook.schedulermacbooktroops.activity;
+
+
+import android.view.View;
+
+import com.example.hyunwook.schedulermacbooktroops.app.BaseActivity;
+import com.example.hyunwook.schedulermacbooktroops.R;
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void bindView() {
+        setContentView(R.layout.activity_main);
+    }
+}
