352ac5783866bb2ec4c654366beb983249afa0c2
hyunwook <c004245@naver.com>
2018-06-30 16:33:41 +0900

    스케줄 내용 작성 액티비티 스케줄 내용, 시간, 카테고리, 위치 를 적을 수 있는 화면

--- app/src/main/java/com/example/hyunwook/schedulermacbooktroops/activity/ScheduleDetailActivity.java
+++ app/src/main/java/com/example/hyunwook/schedulermacbooktroops/activity/ScheduleDetailActivity.java
@@ -1,7 +1,68 @@
 package com.example.hyunwook.schedulermacbooktroops.activity;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.common.base.app.BaseActivity;
+import com.example.common.bean.EventSet;
+import com.example.common.bean.Schedule;
+import com.example.common.listener.OnTaskFinishedListener;
+import com.example.hyunwook.schedulermacbooktroops.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 18-06-29
+ * 스케줄을 위치 등등, 자세하게 적을수있는 Activity
  */
-public class ScheduleDetailActivity {
+public class ScheduleDetailActivity extends BaseActivity implements View.OnClickListener,
+        OnTaskFinishedListener<Map<Integer, EventSet>> {
+
+    private View vSchedule;
+    private ImageView ivEventIcon;
+    private EditText etTitle, etDesc;
+
+    private TextView tvEventSet, tvTime, tvLocation;
+    private Map<Integer, EventSet> mEventSetsMap;
+
+    private Schedule mSchedule;
+    public static String SCHEDULE_OBJ = "schedle.obj";
+    public static String CALENDAR_POSITION = "calendar.position";
+
+    private int mPosition = -1;
+    @Override
+    protected void bindView() {
+        setContentView(R.layout.activity_schedule_detail);
+        TextView tvTitle = searchViewById(R.id.tvTitle);
+        tvTitle.setText(getString(R.string.schedule_event_detail_setting));
+
+        searchViewById(R.id.tvCancel).setOnClickListener(this);
+        searchViewById(R.id.tvFinish).setOnClickListener(this);
+        searchViewById(R.id.llScheduleEventSet).setOnClickListener(this);
+        searchViewById(R.id.llScheduleTime).setOnClickListener(this);
+        searchViewById(R.id.llScheduleLocation).setOnClickListener(this);
+
+        vSchedule = searchViewById(R.id.vScheduleColor);
+        ivEventIcon = searchViewById(R.id.ivScheduleEventSetIcon);
+
+        etTitle = searchViewById(R.id.etScheduleTitle);
+        etDesc = searchViewById(R.id.etScheduleDesc);
+
+        tvEventSet = searchViewById(R.id.tvScheduleEventSet);
+        tvTime = searchViewById(R.id.tvScheduleTime);
+        tvLocation = searchViewById(R.id.tvScheduleLocation);
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        mEventSetsMap = new HashMap<>();
+        mSchedule = (Schedule)getIntent().getSerializableExtra(SCHEDULE_OBJ);
+        mPosition = getIntent().getIntExtra(CALENDAR_POSITION, -1);
+
+        new LoadEventSetMapTask(this, this).exe
+    }
 }
