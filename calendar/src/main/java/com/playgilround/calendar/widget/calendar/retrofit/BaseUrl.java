76f3fc84b1de307e05496e0b429d7a9789ac8f7f
hyunwook <c004245@naver.com>
2018-10-16 00:07:42 +0900

    [#160646158] 스케줄 CRUD 관련 BaseUrl 추가

--- calendar/src/main/java/com/playgilround/calendar/widget/calendar/retrofit/BaseUrl.java
+++ calendar/src/main/java/com/playgilround/calendar/widget/calendar/retrofit/BaseUrl.java
@@ -22,8 +22,6 @@ public class BaseUrl {
 
     public static final String PATH_FRIEND_SEARCH = "/v1/friends";
 
-    public static final String PATH_NEW_SCHEDULE = "/v1/schedule/new";
-
     public static final String PATH_FIND_EMAIL = "v1/user/email";
 
     public static final String PATH_FIND_PASSWORD = "v1/user/password";
@@ -31,6 +29,23 @@ public class BaseUrl {
     public static final String PATH_RESET_PASSWORD = "/v1/user/password/new";
 
     public static final String PATH_FRIEND_ASSENT = "/v1/friends/{pushId}/consent";
+
+
+    //schedule
+    public static final String PATH_UPDATE_SCHEDULE = "/v1/schedule/edit/{scheId}";
+
+    public static final String PATH_NEW_SCHEDULE = "/v1/schedule/new";
+
+    public static final String PATH_SEARCH_SCHEDULE = "/v1/schedule";
+
+    public static final String PATH_DETAIL_SCHEDULE = "/v1/schedule/{scheId}";
+
+    public static final String PATH_DELETE_SCHEDULE = "/v1/schedule/{scheId}";
+
+    public static final String PATH_ARRIVE_SCHEDULE = "/v1/schedule/{scheId}/arrive";
+
+    public static final String PATH_ASSENT_SCHEDULE = "/v1/schedule/{scheId}/assent";
+
 
     // Note:: Params
     public static final String PARAM_YEAR = "year";
