cb3eb542b3a326616f09a9cde7f908b8e5be8310
hyunwook <c004245@naver.com>
2018-11-12 18:42:16 +0900

    [#160890259] 위치 다이얼로그 에 내 위치 지도 표시 작업

--- app/src/main/java/com/playgilround/schedule/client/dialog/InputLocationDialog.java
+++ app/src/main/java/com/playgilround/schedule/client/dialog/InputLocationDialog.java
@@ -1,17 +1,29 @@
 package com.playgilround.schedule.client.dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.playgilround.schedule.client.R;
+import com.playgilround.schedule.client.activity.MainActivity;
 
 /**
  * 18-07-05
  * 위치 설정 다이얼로그
  */
-public class InputLocationDialog extends Dialog implements View.OnClickListener{
+public class InputLocationDialog extends Dialog implements View.OnClickListener, OnMapReadyCallback {
+
+    static final String TAG = InputLocationDialog.class.getSimpleName();
 
     private OnLocationBackListener mOnLocationBackListener;
     private EditText etLocationContent;
@@ -24,11 +36,33 @@ public class InputLocationDialog extends Dialog implements View.OnClickListener{
 
     private void initView() {
         setContentView(R.layout.dialog_input_location);
-        etLocationContent = (EditText) findViewById(R.id.etLocationContent);
+//        etLocationContent = (EditText) findViewById(R.id.etLocationContent);
         findViewById(R.id.tvCancel).setOnClickListener(this);
         findViewById(R.id.tvConfirm).setOnClickListener(this);
+        Activity activity = MainActivity.getActivity();
+        FragmentManager fragmentManager = activity.getFragmentManager();
+
+        Log.d(TAG, "fragment state -> " + fragmentManager);
+        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
+        Log.d(TAG, "fragment state 2-> " + mapFragment);
+
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(final GoogleMap map) {
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        map.addMarker(markerOptions);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
 
     @Override
     public void onClick(View v) {
