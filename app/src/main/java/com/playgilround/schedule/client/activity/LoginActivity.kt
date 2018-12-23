18f436cc22cffc627e956c0b9f76bd16e25072ae
hyunwook <c004245@naver.com>
2018-12-23 14:09:33 +0900

    [#162826996] Retrofit Holiday 부분 코틀린 변경

--- app/src/main/java/com/playgilround/schedule/client/activity/LoginActivity.kt
+++ app/src/main/java/com/playgilround/schedule/client/activity/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.playgilround.schedule.client.activity
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.SharedPreferences
+import android.os.AsyncTask.execute
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -10,6 +12,9 @@ import android.widget.CheckBox
 import android.widget.EditText
 import com.facebook.CallbackManager
 import com.facebook.login.widget.LoginButton
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import com.kakao.auth.ErrorCode
@@ -24,8 +29,18 @@ import com.kakao.util.exception.KakaoException
 import com.playgilround.schedule.client.R
 import com.playgilround.schedule.client.dialog.SelectFindDialog
 import com.playgilround.schedule.client.facebook.LoginCallback
+import com.playgilround.schedule.client.gson.HolidayJsonData
+import com.playgilround.schedule.client.realm.ScheduleR
+import com.playgilround.schedule.client.retrofit.APIClient
+import com.playgilround.schedule.client.retrofit.APIInterface
 import io.realm.Realm
 import io.realm.RealmList
+import io.realm.RealmResults
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.lang.reflect.Type
 import java.util.*
 
 /**
@@ -93,10 +108,99 @@ class LoginActivity : Activity, SelectFindDialog.OnFindSetListener {
         callback = SessionCallback()
         Session.getCurrentSession().addCallback(callback)
         requestMe()
+
+        mCallbackManager = CallbackManager.Factory.create()
+        mLoginCallback = LoginCallback()
+
+        pref = getSharedPreferences("loginData", Context.MODE_PRIVATE)
+        editor = pref.edit()
+
+        idInput = findViewById(R.id.idInput)
+        pwInput = findViewById(R.id.pwInput)
+
+        autoCheck = findViewById(R.id.checkbox)
+        registBtn = findViewById(R.id.registBtn)
+
+        loginBtn = findViewById(R.id.loginBtn)
+        findBtn = findViewById(R.id.findBtn)
+
+        btn_facebook = findViewById(R.id.btn_facebook_login)
+        btn_facebook.setReadPermissions(Arrays.asList("public_profile", "email"))
+        btn_facebook.registerCallback(mCallbackManager, mLoginCallback)
+
+        realm = Realm.getDefaultInstance()
+
+        /**
+         * Retrofit Holiday
+         */
+        check
+    }
+
+    fun checkHoliday(callback : ApiCallback) {
+        var nYear: Int
+
+        val calendar: Calendar = GregorianCalendar(Locale.KOREA)
+        nYear = calendar.get(Calendar.YEAR)
+
+        Log.d(TAG, "checkHoliday -> $nYear")
+
+        val retrofit: Retrofit = APIClient.getClient()
+        val holidayAPI = retrofit.create(APIInterface::class.java)
+        val result : Call<ArrayList<JsonObject>> = holidayAPI.getListHoliday(nYear)
+        result.enqueue(object : Callback<ArrayList<JsonObject>> {
+            override fun onResponse(call: Call<ArrayList<JsonObject>>, response: Response<ArrayList<JsonObject>>) {
+                Log.d(TAG, "checkResponse ------- " + response.code())
+
+                if (response.isSuccessful && response.body() != null) {
+                    realm.executeTransaction {
+                        Log.d(TAG, "Check Holdiay ")
+                        val holidayRS = realm.where(ScheduleR::class.java).equalTo("eventSetId", -1).findAll()
+                        if (holidayRS.size == 0) {
+                            //ScheduleR Table 에 공휴일정보가 저장이 되어있지 않음
+                            /**
+                             * Use gson json parsing..
+                             */
+                            val list = object : TypeToken<List<HolidayJsonData>>(){}.type
+                            var holidays: List<HolidayJsonData> = Gson().fromJson(response.body().toString(), list)
+
+                            for (resHoliday: HolidayJsonData in holidays) {
+                                val currentIdNum = realm.where(ScheduleR::class.java).max("seq")
+
+                                val nextId: Int
+
+                                if (currentIdNum == null) {
+                                    nextId = 0
+                                } else {
+                                    nextId = currentIdNum.toInt() + 1
+                                }
+
+                                var holidayR: ScheduleR = realm.createObject(ScheduleR::class.java, nextId)
+
+                                holidayR.id = resHoliday.id
+                                holidayR.year = resHoliday.year
+                                holidayR.month = resHoliday.month
+                                holidayR.day = resHoliday.day
+                                holidayR.title = resHoliday.name
+                                holidayR.eventSetId = -1 //공휴일 EventSetId -1 고정
+                                holidayR.color = -1 //공휴일 하늘색.
+                                holidayR.state = -1 //상태 -1 (좌측 노란색)
+                            }
+                        } else {
+                        }
+                        callback.onSuccess("success")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<JsonObject>>, t: Throwable) {
+                Log.d(TAG, "error ->" + t.toString())
+            }
+        })
+
     }
 
     //카카오톡 유저정보 관련
-    public fun requestMe() {
+    fun requestMe() {
         //유저의 정보를 받아오는 함수
         UserManagement.requestMe(object : MeResponseCallback() {
             override fun onSessionClosed(errorResult: ErrorResult?) {
@@ -159,6 +263,12 @@ class LoginActivity : Activity, SelectFindDialog.OnFindSetListener {
         override fun onSessionOpenFailed(exception: KakaoException?) {
             Log.d(TAG, "Kakao onSessionOpenFail...")
         }
+    }
+
+
+    interface ApiCallback {
+        fun onSuccess(result: String)
+        fun onFail(result: String)
     }
 
 
